53acd5d40f8aa9fd2fc1645f768adff46af5fb7d
uttam9721 <uttammaurya377@gmail.com>
2026-02-07 23:33:11 +0530

    fix cartCount

--- client/src/context/ShopContext.jsx
+++ client/src/context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { createContext,useState } from "react";
 import { products } from "../assets/assets";
 
@@ -9,17 +9,47 @@ const ShopContextProvider = (props) => {
   const delivery_fee = 10;
   const [search,setSearch]=useState('');
   const [showSearch,setShowSearch]=useState(false)
-  // const [cartItems,setCartItems]=useState({});
+  const [cartItems,setCartItems]=useState({});
 
 
-  // const addToCart=async(itemId,size)=>{
-  //   let cartData=structuredClone(cartItems);
-  //   if(cartData[itemId]){
-  //     if(cartData[itemId])
-  //   }
+  const addToCart=async(itemId,size)=>{
+    let cartData=structuredClone(cartItems);
+    if(cartData[itemId]){
+      if(cartData[itemId]){
+        cartData[itemId][size]+=1;
+      }else{
+        cartData[itemId][size]=1;
+      }
+    }
+    else{
+      cartData[itemId]={};
+      cartData[itemId][size]=1;
+    }
+    setCartItems(cartData)
 
-  // }
+  }
 
+  const getCartCount=()=>{
+    let totalCount=0;
+    for(const items in cartItems){
+      for(const item in cartItems[items]){
+        try{
+          if(cartItems[items][item]>0){
+            totalCount+=cartItems[items][item];
+          }
+        }catch(error){
+          console.log(error);
+          
+
+        }
+      }
+    }
+    return totalCount;
+  }
+
+  useEffect(()=>{
+
+  },[cartItems])
   const value = {
     products,
     currency,
@@ -28,6 +58,9 @@ const ShopContextProvider = (props) => {
     setSearch,
     showSearch,
     setShowSearch,
+    cartItems,
+    addToCart,
+    getCartCount
 
   };
 
