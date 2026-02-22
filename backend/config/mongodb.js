549bba262ed4102b96ef8980a9ee8f103477dbfd
uttam9721 <uttammaurya377@gmail.com>
2026-02-22 23:34:58 +0530

    done

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,32 +1,13 @@
-// import mongoose from "mongoose";
-
-
-// const connectDB=async()=>{
-    
-//     mongoose.connection.on('connected',()=>{
-//         console.log("DB Connected.....");
-        
-//     })
-//     await mongoose.connect(`${process.env.MONGODB_URI}/modexa`)
-
-// }
-// export default connectDB;
-
-
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
 
-    mongoose.connection.on("connected", () => {
-      console.log("✅ DB Connected...");
-    });
+const connectDB=async()=>{
+    
+    mongoose.connection.on('connected',()=>{
+        console.log("DB Connected.....");
+        
+    })
+    await mongoose.connect(`${process.env.MONGODB_URI}/modexa`)
 
-  } catch (error) {
-    console.error("❌ DB Connection Error:", error);
-    process.exit(1);
-  }
-};
-
+}
 export default connectDB;
