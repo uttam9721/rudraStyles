c2d6152ea2651c7cbce720cf14f70272dcc5c178
uttam9721 <uttammaurya377@gmail.com>
2026-02-22 23:13:29 +0530

    live

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,13 +1,32 @@
+// import mongoose from "mongoose";
+
+
+// const connectDB=async()=>{
+    
+//     mongoose.connection.on('connected',()=>{
+//         console.log("DB Connected.....");
+        
+//     })
+//     await mongoose.connect(`${process.env.MONGODB_URI}/modexa`)
+
+// }
+// export default connectDB;
+
+
 import mongoose from "mongoose";
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
 
-const connectDB=async()=>{
-    
-    mongoose.connection.on('connected',()=>{
-        console.log("DB Connected.....");
-        
-    })
-    await mongoose.connect(`${process.env.MONGODB_URI}/modexa`)
+    mongoose.connection.on("connected", () => {
+      console.log("✅ DB Connected...");
+    });
 
-}
+  } catch (error) {
+    console.error("❌ DB Connection Error:", error);
+    process.exit(1);
+  }
+};
+
 export default connectDB;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
       <img src={assets.logo} alt="logo" className='w-36' />
         </NavLink>
 
-      <ul className='hidden sm:flex gap-5 text-sm text-gray-700 hidden'>
+      <ul className='sm:flex gap-5 text-sm text-gray-700 hidden'>
         <NavLink to={'/'} className='flex flex-col items-center gap-1' >
         <p>HOME</p>
         <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
